bee054599cf74da74be7cb88ce72fc5a0fe1f98b
RionaSexton <145370406+RionaSexton@users.noreply.github.com>
2025-10-04 18:31:13 +0100

    Symbols Script

---
+++ Ritual/Assets/Scripts/SymbolManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class SymbolManager : MonoBehaviour
+{
+    [Header("Five Symbols")]
+    public GameObject triangleSymbol;
+    public GameObject starSymbol;
+    public GameObject squareSymbol;
+    public GameObject eyeballSymbol;
+    public GameObject doubleEyeballSymbol;
+
+    [Header("For reference")]
+    public SymbolType activeSymbolType; 
+    private GameObject activeSymbolObject;
+
+    
+    public enum SymbolType
+    {
+        Triangle,
+        Star,
+        Square,
+        Eyeball,
+        DoubleEyeball
+    }
+
+    private void Start()
+    {
+        
+        GameObject[] symbols = { triangleSymbol, starSymbol, squareSymbol, eyeballSymbol, doubleEyeballSymbol };
+
+        
+        foreach (GameObject symbol in symbols)
+        {
+            if (symbol != null)
+                symbol.SetActive(false);
+        }
+
+        
+        int index = Random.Range(0, symbols.Length);
+        activeSymbolObject = symbols[index];
+        activeSymbolObject.SetActive(true);
+
+        
+        activeSymbolType = (SymbolType)index;
+
+        Debug.Log("Active Symbol: " + activeSymbolType);
+    }
+
+    
+    public SymbolType GetActiveSymbol()
+    {
+        return activeSymbolType;
+    }
+
+    
+    public GameObject GetActiveSymbolObject()
+    {
+        return activeSymbolObject;
+    }
+}

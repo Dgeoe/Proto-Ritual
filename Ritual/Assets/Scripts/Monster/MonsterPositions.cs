ad34eccde4281e17895ae2de3c8157e57082e09c
Dgeoe <joedoshea15@gmail.com>
2025-10-10 18:41:46 +0100

    Quick Monster Changes

--- Ritual/Assets/Scripts/Monster/MonsterPositions.cs
+++ Ritual/Assets/Scripts/Monster/MonsterPositions.cs
@@ -2,15 +2,71 @@ using UnityEngine;
 
 public class MonsterPositions : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject[] Positions;
+    public GameObject miniJumpScare;
+    public GameObject JumpScare;
+    private int howmany;
+    private int holder = 0;
+    private int pickMemory;
+    private int randomPick;
+
+    private void Start()
     {
-        
+        if (Positions != null)
+        {
+            RandomPosition();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RandomPosition()
     {
-        
+        foreach (GameObject i in Positions)
+        {
+            i.SetActive(false);
+        }
+        howmany = Positions.Length; 
+        randomPick = Random.Range(0, howmany - 1);
+        if (holder == 0)
+        {
+            pickMemory = randomPick;
+            holder++;
+        }
+        else
+        {
+            if (randomPick == pickMemory)
+            {
+                randomPick = Random.Range(0, howmany - 1);
+                RandomPosition();
+            }
+            else
+            {
+                holder = 0;
+            }
+        }
+        Positions[randomPick].SetActive(true);
+    }
+
+    private void MiniJumpscare()
+    {
+        //Get Player Position 
+        //TP GameObject to be infront of that position
+        //Set Salt Circle Spawn Limits
+    }
+
+    public void ForSeeingGlass()
+    {
+        int chance = Random.Range(0, 4);
+        if (chance == 0)
+        {
+            MiniJumpscare();
+        }
+        else
+        {
+            RandomPosition();
+        }
+    }
+    public void Jumpscare()
+    {
+
     }
 }
